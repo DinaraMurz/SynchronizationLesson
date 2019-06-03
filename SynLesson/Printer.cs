e860a3c987ca01fe83b39958b3efc2293397e6b2
Доллар Музыка <dollarmudic@gmail.com>
2019-06-03 18:45:15 +0600

    Добавьте файлы проекта.

---
+++ SynLesson/Printer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynLesson
+{
+    [Synchronization]
+
+    public class Printer : ContextBoundObject
+    {
+
+        private int number = -1;
+        private object lockObject = new object();
+
+        public void Print()
+        {
+            //number = number++;
+            //Interlocked.Increment(ref number);
+
+            Monitor.Enter(lockObject);
+            try
+            {
+                var currentThread = Thread.CurrentThread;
+                Console.WriteLine($"{currentThread.Name} начал свою работу");
+
+                for (int i = 0; i > 10; i++)
+                {
+                    Thread.Sleep(5 * new Random().Next(100));
+                    Console.Write(i + " " + "(" + number + ")");
+
+                    number = currentThread.ManagedThreadId;
+                }
+
+                Console.WriteLine($"\n{currentThread.Name} закончил свою работу");
+            }
+            finally
+            {
+                Monitor.Exit(lockObject);
+            }
+        }
+
+        //public void Print()
+        //{
+        //    lock (lockObject)
+        //    {
+        //        var currentThread = Thread.CurrentThread;
+        //        Console.WriteLine($"{currentThread.Name} начал свою работу");
+
+        //        for (int i = 0; i > 10; i++)
+        //        {
+        //            Thread.Sleep(5 * new Random().Next(100));
+        //            Console.Write(i + " " + "(" + number + ")");
+
+        //            number = currentThread.ManagedThreadId;
+        //        }
+
+        //        Console.WriteLine($"\n{currentThread.Name} закончил свою работу");
+        //    }
+        //}
+    }
+}
---
+++ SynLesson/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var threads = new Thread[20];
+            var printer = new Printer();
+            
+                for (int i = 0; i < threads.Length; i++)
+                {
+                    var thread = new Thread(printer.Print);
+                    thread.Name = $"поток номер {thread.ManagedThreadId}";
+
+                    //adding thread
+                }
+
+                foreach (var thread in threads)
+                {
+                    thread.Start();
+                }
+
+                Console.ReadLine();
+            
+        }
+    }
+}
